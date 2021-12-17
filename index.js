104ab9ef4061505d26209718d2c13459a17931c5
hritvik <hritvik.agarwal@gmail.com>
2021-12-16 21:29:35 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// set up our express app
+const app = express();
+
+uri =
+	"mongodb+srv://hritvik:hritvik@skrate-assignment.ioawu.mongodb.net/skrate-assignment?retryWrites=true&w=majority";
+
+// connect to mongodb
+mongoose.connect(uri);
+mongoose.Promise = global.Promise;
+
+app.use(express.static('public'));
+
+app.get("/",function(req,res){
+    res.send("Hi!")
+})
+
+app.use(express.json());
+// initialize routes
+app.use('/api',require('./routes/api'));
+
+// error handling middleware
+app.use(function(err,req,res,next){
+    //console.log(err);
+    res.status(422).send({error: err.message});
+});
+
+// listen for requests
+app.listen(process.env.port || 5000, function(){
+    console.log('Ready to Go!');
+});
---
+++ models/meetings.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MeetingSchema = new Schema({
+	uid1: {
+		type: Schema.Types.ObjectId,
+        ref: "users"
+	},
+    uid2: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Meeting = mongoose.model("meetings", MeetingSchema);
+
+module.exports = Meeting;
